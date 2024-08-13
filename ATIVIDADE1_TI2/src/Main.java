f7d7e8bd8a5fb417e441b3fa9194787af6b33e78
Pedro-HFelix <pf711363@gmail.com>
2024-08-13 19:59:30 -0300

    organização

--- ATIVIDADE1_TI2/src/Main.java
+++ ATIVIDADE1_TI2/src/Main.java
@@ -4,11 +4,14 @@ public class Main {
 	public static Scanner sc = new Scanner(System.in); 
 	
 	public static void main(String[] args) {
+		int num1, num2, total;
 		System.out.println("Digite o primeiro número ");
-		int num1 = sc.nextInt();
+		num1 = sc.nextInt();
+
 		System.out.println("Digite o segundo número ");
-		int num2 = sc.nextInt();
-		int total = num1 + num2;
+		num2 = sc.nextInt();
+		
+		total = num1 + num2;
 		System.out.println("Total: " + total);
 	}
 }
